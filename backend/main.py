50d559f63e85056c8491904a2266a0ba8cb8b125
LussanPH <pedrohenriquelunalessa@gmail.com>
2026-08-19 13:01:23 -0300

    Removed ngrok path to run inm local host

--- backend/main.py
+++ backend/main.py
@@ -7,24 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 
-#FAZ COM QUE O TÚNEL SEJA ABERTO APENAS UMA VEZ DURANTE A EXECUÇÃO DO UVICORN
-@asynccontextmanager
-async def lifespan(app : FastAPI):
-    ngrok.set_auth_token(NGROK_TOKEN)
-
-    tunnel = ngrok.connect(
-        PORT,
-        domain=NGROK_DOMAIN
-    )
-
-    print(f"Tunnel opened with success! {tunnel.public_url}")
-
-    yield
-
-    ngrok.disconnect(public_url=tunnel.public_url)
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
@@ -44,7 +27,7 @@ app.include_router(ubs_router)
 app.include_router(cm_router)
 app.include_router(auth_router)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=PORT, reload=True)#uvicorn main:app --host="127.0.0.1" --port 8000 --reload
+#if __name__ == "__main__":
+    #uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)#uvicorn main:app --host="127.0.0.1" --port 8000 --reload
 
 
